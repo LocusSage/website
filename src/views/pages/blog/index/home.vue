5fea2e1eb45930e1fa9ad5de7ef577b1531b87d6
Lorena Evêncio Enoque <lorena@mpscloud.com.br>
2018-10-18 16:38:31 -0300

    [ADD] components in the blog index

---
+++ src/views/pages/blog/index/home.vue
@@ -0,0 +1,28 @@
+<template>
+<div class="grid full-width grid-template">
+  <carrousel-post/>
+  <carrousel/>
+  <post-resume/>
+  <subscribe/>
+</div>
+</template>
+<script>
+ 
+import carrouselPost from './components/carrousel-post'
+import carrousel from './components/carrousel'
+import postResume from './components/post-resume'
+import subscribe from './components/subscribe'
+
+export default {
+   components: {
+     'carrousel-post': carrouselPost,
+     'carrousel': carrousel,
+     'post-resume': postResume,
+     'subscribe': subscribe
+   }
+}
+</script>
+<style scoped lang="scss">
+
+</style>
+
